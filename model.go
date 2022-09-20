5f2229e23256f3a30430639b5b5617bc1e60131f
zaqxsw800402 <zaqxsw79218@gmail.com>
2022-09-20 18:42:02 +0800

    first commit

---
+++ model.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type Tracking struct {
+	ID          int64
+	Referrer    string
+	Platform    string
+	Event       string
+	EventSource string
+	Object      string
+	Uid1        string
+	Uid2        string
+	IP          string
+	Version     string
+	CreatedAt   time.Time
+}
