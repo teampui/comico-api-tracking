2ec40b4ea4b79372d0e1ac75839512d1c94907a0
zaqxsw800402 <zaqxsw79218@gmail.com>
2022-09-22 15:30:07 +0800

    change log

--- model.go
+++ model.go
@@ -32,17 +32,17 @@ var (
 func init() {
 	key = os.Getenv("API_KEY")
 	if key == "" {
-		panic("TRACKING_KEY is not set")
+		panic("API_KEY is not set")
 	}
 
 	secret = os.Getenv("API_SECRET")
 	if secret == "" {
-		panic("TRACKING_SECRET is not set")
+		panic("API_SECRET is not set")
 	}
 
 	host = os.Getenv("API_HOST")
 	if host == "" {
-		panic("TRACKING_HOST is not set")
+		panic("API_HOST is not set")
 	}
 }
 
